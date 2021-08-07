227f1ef11b7d4f244ec3eddc8528c03e03ed1420
Iamdheeraj21 <dheerajprajapati334@gmail.com>
2021-08-07 17:13:06 +0530

    UI Designed!

--- app/src/main/java/com/example/textdetector/MainActivity.java
+++ app/src/main/java/com/example/textdetector/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.textdetector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.captureImageButton);
+        button.setOnClickListener(v->{
+            Intent intent=new Intent(MainActivity.this,ScannerActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
     }
 }
